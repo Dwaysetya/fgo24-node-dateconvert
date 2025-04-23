f454f1c7d01709c876bf75f1ded96347a11a22a6
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-24 01:36:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,23 +5,69 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-// console.log(moment("02-08-1999", "DD-MM-YYYY").format("DD/MM/YYYY"));
 
 function menuUtama() {
   console.log("Silahkan Masukka Tanggal \n");
 
   rl.question("Masukkan Tanggal: ", (jawaban) => {
     updateDate(jawaban);
-    rl.close;
+    // dateManual(jawaban);
+    rl.close();
   });
 }
 menuUtama();
 
 const updateDate = (date) => {
-  if (moment(date, "DD-MM-YYYY").isValid) {
-    const result = moment(date, "DD-MM-YYYY").format("DD/MM/YYYY");
-    console.log(result);
-  } else {
-    console.log("format tanggal harus DD-MM-YYYY");
+  try {
+    const result = moment(date, "DD-MM-YYYY", true);
+    if (result.isValid()) {
+      const newResult = result.format("DD/MM/YYYY");
+      console.log(`Nahhhh, ini baru bener. ${newResult}`);
+      return;
+    } else {
+      const error = new Error("input_salah");
+      throw error;
+    }
+  } catch (a) {
+    if (a.message === false) {
+      console.log("Format salah");
+    }
+    if (a.message === "input_salah") {
+      console.log("salah..., harusnya DD/MM/YYYY, formatnya ");
+    }
   }
 };
+
+// const dateManual = (date) => {
+//   try {
+//     const newDate = date.trim().split("-");
+//     if (newDate.length !== 3) {
+//       throw new Error("Format_salah");
+//     }
+
+//     const [day, month, years] = newDate;
+
+//     if (
+//       day.length < 1 ||
+//       day.length > 2 ||
+//       month.length < 1 ||
+//       month.length > 2 ||
+//       years.length !== 4
+//     ) {
+//       throw new Error("input_string");
+//     }
+
+//     const joinDate = newDate.join("/");
+//     console.log(`nahh ini baru bener, Hasil format ulang anda ${joinDate}`);
+//   } catch (error) {
+//     if (error.message === "Format_salah") {
+//       console.log("Format harus DD-YY-MMMM");
+//     } else if (error.message === "input_string") {
+//       console.log("coba di cek lagi, format anda salah, harus DD-YY-MMMM");
+//     } else {
+//       console.log("input invalid");
+//     }
+//   }
+// };
+
+
