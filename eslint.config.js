a9414dc2d9dcdef8fab7379e541cda41d8cc490e
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-23 14:34:35 +0700

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {plugins: {
+    '@stylistic/js': stylisticJs
+  },
+},
+  {
+    rules: {
+      semi: ["error", "always"],
+      indent : [
+        "error",
+        "tab"
+    ],
+    indent: ['error', 2],
+      '@stylistic/js/indent': ['error', 2],
+    },
+    linterOptions: {
+			reportUnusedInlineConfigs: "error",
+		},
+    languageOptions: {
+			ecmaVersion: 5,
+			sourceType: "script",
+		},
+    
+  },
+]);
